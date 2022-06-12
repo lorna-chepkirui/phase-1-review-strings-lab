f1cb89391226b7f3f24adeaaf4414576323d74e2
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-12 14:38:49 +0300

    commit tree 34144411574a9c82f15fab138ff108fb71b22bca

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser}!`.toLocaleUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
